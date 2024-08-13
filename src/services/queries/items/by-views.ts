8ea6dd7b6d7db1a93617053e668e8330c65fcc36
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-08-14 00:28:30 +0300

    Refactor item query by views to include additional fields and improve sorting
    The `itemsByViews` function has been updated to fetch additional item properties such as `endingAt`, `imageUrl`, and `price` during the query process. This change enhances the data retrieval efficiency by reducing the need for subsequent database calls to gather these details. The sorting mechanism has also been adjusted to use the `nosort` option with explicit direction control, allowing for more predictable and customizable ordering based on the `order` parameter.
    
    Furthermore, the results are now processed in a more structured manner, leveraging the `deserialize` function to format each item entry correctly. This approach ensures that the returned items are consistently structured and easier to work with in downstream applications. The changes also include a refactoring of the `GET` array to be more readable and maintainable, improving the overall code quality and readability.

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,9 +1,33 @@
 import { itemsByViewsKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-	const results = await client.sort(itemsByViewsKey(), {
-		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
-		BY: 'score'
+	let results: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
+		BY: 'nosort',
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
 	});
+
+	const items = [];
+
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
 };
