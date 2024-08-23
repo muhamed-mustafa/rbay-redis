ad79ac75e18d4becd83906ce2f4a04beed1dd9d2
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-08-23 22:42:45 +0300

    Refactor Redis client and session handling for efficiency and debugging
    - Introduced custom Redis scripts for incrementing views and adding values, enhancing performance and reducing network latency.
    - Added logging in session retrieval for debugging purposes.
    - Simplified view increment logic by leveraging the new Redis script, streamlining the process and reducing code complexity.
    - Configured Redis client to use environment variables for connection details, ensuring flexibility and security.
    - Established a connection event listener to test the new script functionality upon connection, aiding in early issue detection.

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -3,6 +3,8 @@ import { sessionsKey } from '$services/keys';
 import { client } from '$services/redis';
 
 export const getSession = async (id: string) => {
+	console.log("client",client);
+	
 	const session = await client.hGetAll(sessionsKey(id));
 
 	if (Object.keys(session).length === 0) return null;
