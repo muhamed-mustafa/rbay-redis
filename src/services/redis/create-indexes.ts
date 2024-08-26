82db8f0c23a76a75ceefe63b192deff7259b698c
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-08-27 00:11:11 +0300

    Add Redis Index for Items
    Created a new Redis index for items to enhance search capabilities. The index, named `idx:items`, includes fields for `name` and `description` of items, both treated as text for full-text search purposes. This enhancement allows for more efficient querying and retrieval of item data based on textual attributes. The index creation function `createIndexes` in the `create-indexes.ts` file has been updated to include this new index, specifying the `ON` strategy as `HASH` and the `PREFIX` as `items#` to match the existing data structure.

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,1 +1,16 @@
-export const createIndexes = async () => {};
+import { client } from '$services/redis';
+import { itemsIndexKey } from '$services/keys';
+import { SchemaFieldTypes } from 'redis';
+
+export const createIndexes = async () => {
+	return (
+		client.ft.create(itemsIndexKey(), {
+			name: { type: SchemaFieldTypes.TEXT },
+			description: { type: SchemaFieldTypes.TEXT }
+		}),
+		{
+			ON: 'HASH',
+			PREFIX: 'items#'
+		}
+	);
+};
