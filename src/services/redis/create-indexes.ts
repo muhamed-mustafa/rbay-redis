07ba33e371e92d35d4d9931d50826456502bf9f7
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-08-31 17:10:50 +0200

    Refactor item queries and Redis index creation for enhanced search and sorting
    This commit introduces significant enhancements to the item querying and search functionalities within our application. Specifically, it refactors the `itemsByUser` and `searchItems` functions to leverage Redis's full-text search capabilities, enabling more efficient and flexible querying and sorting of items.
    
    The `itemsByUser` function now supports pagination and sorting based on various criteria such as ownerId, endingAt, bids, views, and price. This is achieved by integrating Redis's search functionality with the application's existing Redis client, allowing for dynamic sorting and pagination of items owned by a specific user.
    
    Similarly, the `searchItems` function has been updated to perform full-text searches on item names and descriptions, with the ability to weight matches in the name field more heavily. This enhancement improves the relevance of search results and provides a better user experience.
    
    Additionally, this commit includes improvements to the Redis index creation process. The `createIndexes` function now checks if the necessary index already exists before attempting to create it, preventing potential errors and ensuring that the index is only created once. The index schema has been expanded to include fields for ownerId, endingAt, bids, views, and price, all of which are now sortable, providing greater flexibility in querying and sorting item data.
    
    These changes collectively aim to improve the performance and usability of item querying and searching within the application, leveraging Redis's powerful indexing and search capabilities.

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -3,11 +3,21 @@ import { itemsIndexKey } from '$services/keys';
 import { SchemaFieldTypes } from 'redis';
 
 export const createIndexes = async () => {
+	const indexs = await client.ft._list();
+	const exists = indexs.find((index) => index === itemsIndexKey());
+
+	if (exists) return;
+
 	return (
 		client.ft.create(itemsIndexKey(), {
-			name: { type: SchemaFieldTypes.TEXT },
-			description: { type: SchemaFieldTypes.TEXT }
-		}),
+			name: { type: SchemaFieldTypes.TEXT, SORTABLE: true },
+			description: { type: SchemaFieldTypes.TEXT, SORTABLE: false },
+			ownerId: { type: SchemaFieldTypes.TAG, SORTABLE: false },
+			endingAt: { type: SchemaFieldTypes.NUMERIC, SORTABLE: true },
+			bids: { type: SchemaFieldTypes.NUMERIC, SORTABLE: true },
+			views: { type: SchemaFieldTypes.NUMERIC, SORTABLE: true },
+			price: { type: SchemaFieldTypes.NUMERIC, SORTABLE: true }
+		} as any),
 		{
 			ON: 'HASH',
 			PREFIX: 'items#'
